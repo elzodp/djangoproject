92f1b141404c0d4275e1fe2de6398ab2f433f570
elzodp <diopel125@gmail.com>
2022-07-19 23:20:02 +0200

    on a notre page web bien def.

---
+++ merchex/listings/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Band
+
+
+def hello(request):
+
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html',
+                  context={'bands': bands})
+
+
+def about(request):
+
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
